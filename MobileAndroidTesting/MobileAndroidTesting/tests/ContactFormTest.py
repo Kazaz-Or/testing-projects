df90935821f778d86e45f4381eefce9103788e5f
Kazaz-Or <83350680+Kazaz-Or@users.noreply.github.com>
2022-05-18 21:48:33 +0300

    Add screen shots funcitonality to tests

--- MobileAndroidTesting/MobileAndroidTesting/tests/ContactFormTest.py
+++ MobileAndroidTesting/MobileAndroidTesting/tests/ContactFormTest.py
@@ -1,3 +1,5 @@
+import time
+
 from MobileAndroidTesting.base.DriverClass import Driver
 import MobileAndroidTesting.utilities.CustomLogger as log
 from MobileAndroidTesting.base.BasePage import BasePage
@@ -12,6 +14,8 @@ log.info("Launching Application")
 
 base_page.is_displayed("com.code2lead.kwad:id/ContactUs", "id")
 base_page.click_element("com.code2lead.kwad:id/ContactUs", "id")
+base_page.screenshot("ContactUsPage")
+
 base_page.is_displayed("com.code2lead.kwad:id/Et2", "id")
 base_page.send_text("com.code2lead.kwad:id/Et2", "id", "Name")
 base_page.is_displayed("com.code2lead.kwad:id/Et3", "id")
@@ -20,3 +24,11 @@ base_page.is_displayed("com.code2lead.kwad:id/Et6", "id")
 base_page.send_text("com.code2lead.kwad:id/Et6", "id", "Givatayim Rocks, IL")
 base_page.is_displayed("com.code2lead.kwad:id/Et7", "id")
 base_page.send_text("com.code2lead.kwad:id/Et7", "id", "050-555587")
+base_page.is_displayed("com.code2lead.kwad:id/Btn2", "id")
+base_page.screenshot("ContactUsPage_WithText")
+
+base_page.click_element("com.code2lead.kwad:id/Btn2", "id")
+base_page.screenshot("ContactUs_AfterSubmit")
+
+time.sleep(4)
+driver.quit()
