fbf56719e483bbb233d4c721fa7f7ec7b21efddf
Kazaz-Or <or.kazaz@dazn.com>
2023-05-10 21:02:36 +0300

    First tests for Typescript unit tests with Jest

---
+++ TypescriptUnitTestsWithJest/jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/app/**/*.ts'
+  ]
+}
+
+export default config;
