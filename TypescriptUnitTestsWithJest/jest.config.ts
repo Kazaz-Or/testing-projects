f14246ef58f517eaaf960b243532a71dba227574
Kazaz-Or <or.kazaz@dazn.com>
2023-05-14 21:17:10 +0300

    password checker unit tests

--- TypescriptUnitTestsWithJest/jest.config.ts
+++ TypescriptUnitTestsWithJest/jest.config.ts
@@ -1,12 +1,15 @@
 import type { Config } from '@jest/types'
 
+const baseDir = '<rootDir>/src/app/pass_checker'
+const baseTestDir = '<rootDir>/src/tests'
+
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
   collectCoverage: true,
   collectCoverageFrom: [
-    '<rootDir>/src/app/**/*.ts'
+    `${baseDir}/**/*.ts`
   ]
 }
 
